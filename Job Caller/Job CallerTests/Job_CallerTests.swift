7998c797b999bc6f7c970a79002d2bd0eac165e0
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:33:47 -0400

    Add Job Caller/Job CallerTests/Job_CallerTests.swift

---
+++ Job Caller/Job CallerTests/Job_CallerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Job_CallerTests.swift
+//  Job CallerTests
+//
+//  Created by Elliot Williams on 2025-07-18.
+//
+
+import Testing
+@testable import Job_Caller
+
+struct Job_CallerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
