8741ba67f40e665a7cd3b21222efe10d90df37b3
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:33:48 -0400

    Add Job Caller/Job Caller/Job_CallerApp.swift

---
+++ Job Caller/Job Caller/Job_CallerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Job_CallerApp.swift
+//  Job Caller
+//
+//  Created by Elliot Williams on 2025-07-18.
+//
+
+import SwiftUI
+
+@main
+struct Job_CallerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
